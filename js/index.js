15bea21da59d25e5e02fe64e8c149feead9dd02f
Alex <alejandro.gonzalez.7e3@itb.cat>
2021-06-07 13:55:06 +0200

    Creadas las variables necesarias para coger elementos del DOM

--- js/index.js
+++ js/index.js
@@ -1,1 +1,6 @@
-
+const peronsajes = document.querySelector(".personajes");
+const cargarPersonajes = document.querySelector(".cargar-personajes");
+const personajeDummy = document.querySelector(".personaje-dummy");
+const familia = document.querySelector(".familia");
+const matarFamilia = document.querySelector(".matar-familia");
+const mensaje = document.querySelector(".mensaje");
